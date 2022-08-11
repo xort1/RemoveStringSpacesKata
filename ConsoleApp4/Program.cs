66581679437c997f82edbf91dbfcb41b0617dc39
Daniil Gubanov <yranidrop@gmail.com>
2022-08-11 18:55:11 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Solution
+{
+    public static class SpacesRemover
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            NoSpace(input);
+        }
+
+        public static string NoSpace(string input)
+        {
+            string result = input.Replace(" ", "");
+            return result;
+        }
+    }
+}
